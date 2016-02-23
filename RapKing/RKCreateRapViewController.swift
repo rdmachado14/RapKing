2b9f0e2bb7d226137e142e01448e8ec9dd925d6b
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-02-23 11:02:09 -0400

    Arrumando as pastas.

--- RapKing/RKCreateRapViewController.swift
+++ RapKing/RKCreateRapViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
 class RKCreateRapViewController: UIViewController
 {
 
     @IBOutlet weak var textFieldRapTitle: UITextField!
     @IBOutlet weak var textViewRap: UITextView!
+    
+    // variável para fazer referência ao Core Data
+    var rhymes = [NSManagedObject]()
     
     
     override func viewDidLoad()
@@ -31,6 +35,32 @@ class RKCreateRapViewController: UIViewController
     {
         
     }
+    
+    func saveRhyme(name: String) {
+        //1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext:managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+            insertIntoManagedObjectContext: managedContext)
+        
+        //3
+        person.setValue(name, forKey: "name")
+        
+        //4
+        do {
+            try managedContext.save()
+            //5
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+
 
 
 }
