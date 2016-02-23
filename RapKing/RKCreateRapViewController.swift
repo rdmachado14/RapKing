75bfb64f2ede65084054b8f7dc6d93d00275c46a
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-02-23 18:42:04 -0400

    Criando a tela de criar rap junto com a classe.

---
+++ RapKing/RKCreateRapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RKCreateRapViewController.swift
+//  RapKing
+//
+//  Created by Rodrigo Machado on 2/23/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class RKCreateRapViewController: UIViewController
+{
+    
+    
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    
+
+}
