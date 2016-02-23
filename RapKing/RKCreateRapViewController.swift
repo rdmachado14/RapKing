4a1a66feb648078c295ac61f5fbff25559bddbc1
Rodrigo Machado <rodrigodangelo95@icloud.com>
2016-02-23 10:29:21 -0400

    Criando a classe para criar um rap.

---
+++ RapKing/RKCreateRapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RKCreateRapViewController.swift
+//  RapKing
+//
+//  Created by Rodrigo Machado on 2/23/16.
+//  Copyright © 2016 Rodrigo DAngelo Silva Machado. All rights reserved.
+//
+
+import UIKit
+
+class RKCreateRapViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+
+}
